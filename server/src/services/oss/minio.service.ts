6db93acd3a681e5905432735771e001825897b9c
yiyingxuan <1148064128@qq.com>
2025-09-03 22:40:01 +0800

    sec(nginx): 更新 Content-Security-Policy 并增加 MinIO相关安全配置
    - 更新 Content-Security-Policy 以允许本地开发时连接到 MinIO 服务
    - 修改 X-External-Host 和 X-Minio-External-Endpoint 头的值为 $http_host
    - 更新 CORS 配置以允许 MinIO 所需的额外请求头
    - 修改 MinIO 服务的 bucket 策略，增加允许上传、删除和列出对象的权限

--- server/src/services/oss/minio.service.ts
+++ server/src/services/oss/minio.service.ts
@@ -67,7 +67,7 @@ export class MinIOService implements OssService {
       }
     }
 
-    // 设置bucket策略，允许公共读取
+    // 设置bucket策略，允许公共读取和上传
     try {
       const bucketPolicy = {
         Version: '2012-10-17',
@@ -75,8 +75,8 @@ export class MinIOService implements OssService {
           {
             Effect: 'Allow',
             Principal: '*',
-            Action: 's3:GetObject',
-            Resource: `arn:aws:s3:::${this.bucket}/*`
+            Action: ['s3:GetObject', 's3:PutObject', 's3:DeleteObject','s3:ListBucket'],
+            Resource: [`arn:aws:s3:::${this.bucket}/*`, `arn:aws:s3:::${this.bucket}`]
           }
         ]
       };
